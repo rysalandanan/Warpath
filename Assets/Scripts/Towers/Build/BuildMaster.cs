59e13578bd5b32f86fbb3c1c70b7d7f24aca7412
Raphael Salandanan <raphaelyapsalandanan@gmail.com>
2025-02-04 18:04:57 +0800

    Tower mechanics improvement
    Players can now freely build Towers anywhere in the area
    Towers now take damage when an monster collided with it
    Improved Tower shop panel and players now can see which tower selected and where will it be build

--- Assets/Scripts/Towers/Build/BuildMaster.cs
+++ Assets/Scripts/Towers/Build/BuildMaster.cs
@@ -1,23 +1,75 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildMaster : MonoBehaviour
 {
-    [SerializeField] private GameObject towerParent;
-    private Vector3 _selectedLocation;
-    private MonoBehaviour fromScript;
+    [Tooltip("The object that will hold the built Towers")]
+    [SerializeField] private GameObject towerHolder;
 
-    public void GetBuildLocation(Vector3 selectedLocation, MonoBehaviour caller)
+    [Tooltip("Show which Tower is currently Selected")]
+    [SerializeField] private Image towerSelected;
+
+    [Tooltip("Drag the Tower Shop Panel here:")]
+    [SerializeField] private GameObject towerShop;
+
+    private Vector3 _selectedLocation;
+    private GameObject _towerSelected;
+    private Sprite _spriteSelected;
+    private bool isBuilding = false;
+    private Vector3 _mousePosition;
+    private void Update()
     {
-        _selectedLocation = selectedLocation;
-        fromScript = caller;
+        if(isBuilding)
+        {
+            ShowSelectedTower(_spriteSelected);
+            if (Input.GetMouseButtonDown(0))
+            {
+                _selectedLocation = GetMousePosition();
+                BuildTower(_towerSelected);
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                isBuilding = false;
+                ActivateDeactivateTowerImage(false);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            towerShop.SetActive(true);
+        }
+    }
+    public void SelectTower(GameObject selectedTower)
+    {
+        _towerSelected = selectedTower;
+        isBuilding = true;
+    }
+    public void SelectSprite(Sprite selectedSprite)
+    {
+        _spriteSelected = selectedSprite;
+        isBuilding = true;
+    }
+    private void ShowSelectedTower(Sprite selectedSprite)
+    {
+        ActivateDeactivateTowerImage(true);
+        towerSelected.sprite = selectedSprite;
+        var _mousePos = GetMousePosition();
+        towerSelected.transform.position = new Vector3 (_mousePos.x - 1, _mousePos.y, 0f);
     }
     public void BuildTower(GameObject selectedTower)
     {
-        Instantiate(selectedTower, _selectedLocation, Quaternion.identity, towerParent.transform);
-        if(fromScript is BuildLocation buildLocation)
-        {
-            buildLocation.hasBuild = true;
-            buildLocation.CloseTowerShop();
-        }
+        Instantiate(selectedTower, _selectedLocation, Quaternion.identity, towerHolder.transform);
+        isBuilding = false;
+        ActivateDeactivateTowerImage(false);
+    }
+    private void ActivateDeactivateTowerImage(bool yesNo)
+    {
+        towerSelected.gameObject.SetActive(yesNo);
+        Debug.Log(yesNo);
+    }
+    private Vector3 GetMousePosition()
+    {
+        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _mousePosition = new Vector3(mouseWorldPos.x,mouseWorldPos.y, 0f);
+        return _mousePosition;
     }
 }
---
+++ Assets/Scripts/Towers/Health/TowerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+public class TowerHealth : MonoBehaviour
+{
+    [SerializeField] private float healthPoints = 100;
+    [SerializeField] private float damagePerSecond = 1;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Monster"))
+        {
+            TakeDamage(damagePerSecond * Time.deltaTime);
+        }
+    }
+
+    private void TakeDamage(float damageAmount)
+    {
+        healthPoints -= damageAmount;
+        CheckHealth();
+    }
+
+    private void CheckHealth()
+    {
+        Debug.Log("Health: " + healthPoints);
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
